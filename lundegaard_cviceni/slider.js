c772a57d5799ee806a4994f8178359e2fd614290
Zuzana Kroulikova <zuzanakroulikova@gmail.com>
2019-05-02 13:03:52 +0200

    Create slider.js

---
+++ lundegaard_cviceni/slider.js
@@ -0,0 +1,40 @@
+
+            
+var slideIndex =0;
+            var slides = document.getElementsById("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            showSlides();
+
+
+
+            function showSlides() {
+                var i;
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                }
+                slideIndex++;
+                if (slideIndex> slides.length) {
+                    slideIndex = 1;
+                }
+
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                }
+
+                slides[slideIndex-1].style.display = "block";
+                dots[slideIndex-1].className += " active";
+                setTimeout(showSlides, 8000);
+            }
+
+            function currentSlide(no) {
+                var i;
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                }
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                }
+                slideIndex = no;
+                slides[no-1].style.display = "block";
+                dots[slideIndex-1].className += " active";
+           }
